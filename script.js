e74cc0f4871db4f3e875a040cb257adb3d67189f
atharvashirodkar <atharva.shirodkar.12@gmail.com>
2026-03-26 20:44:33 +0530

    feat: implement task creation, task count, local storage, and modal logic

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
+let tasksData = {};
+
 const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
+const columns = [todo, progress, done];
 
 let dragElement = null;
+
+function addTask(title, desc, column) {
+    const div = document.createElement('div');
+
+    div.classList.add('task');
+    div.setAttribute('draggable', 'true');
+
+    div.innerHTML = `
+        <h2>${title}</h2>
+        <p>${desc}</p>
+        <button>Delete</button>
+    `;
+
+    column.appendChild(div);
+
+    div.addEventListener('drag', (e) => {
+        dragElement = div;
+    })
+
+    return div;
+}
+
+function updateTaskCount() {
+    columns.forEach(col => {
+        const tasks = col.querySelectorAll('.task');
+        const count = col.querySelector('.right');
+
+        tasksData[col.id] = Array.from(tasks).map(t => {
+            return {
+                title: t.querySelector('h2').innerHTML,
+                desc: t.querySelector('p').innerHTML
+            }
+        });
+
+        localStorage.setItem('tasks', JSON.stringify(tasksData));
+        count.innerHTML = tasks.length;
+    })
+}
+
+if (localStorage.getItem('tasks')) {
+    const data = JSON.parse(localStorage.getItem('tasks'));
+
+    for (const col in data) {
+        const column = document.querySelector(`#${col}`);
+
+        data[col].forEach(task => {
+            addTask(task.title, task.desc, column);
+        })
+    }
+    updateTaskCount();
+}
 
 const tasks = document.querySelectorAll('.task');
 
@@ -29,13 +83,42 @@ function addEventOnColumn(column) {
     })
     column.addEventListener('drop', (e) => {
         e.preventDefault();
-        console.log('Dropped', dragElement, column);
+
         column.appendChild(dragElement);
         column.classList.remove('hover-over');
+
+        updateTaskCount();
+
     })
+
 }
 
 addEventOnColumn(todo);
 addEventOnColumn(progress);
 addEventOnColumn(done);
 
+/* Modal Related Logic */
+
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal")
+const addTaskButton = document.querySelector("#add-new-task");
+
+toggleModalButton.addEventListener('click', () => {
+    modal.classList.add('active');
+})
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('active');
+})
+
+addTaskButton.addEventListener('click', () => {
+    const taskTitle = document.querySelector('#task-title-input').value;
+    const taskDesc = document.querySelector('#task-desc-input').value;
+
+    addTask(taskTitle, taskDesc, todo);
+
+    updateTaskCount();
+
+    modal.classList.remove('active');
+});
